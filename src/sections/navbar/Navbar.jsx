8aff559e766efc99425af55f312191e2875009f3
EngNelson <bechem200@gmail.com>
2022-12-20 22:01:16 +0100

    navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
+import Logo from "../../assets/yaweh.jpg";
+import data from "./data";
+import { IoMdColorPalette } from "react-icons/io";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
-      <h2>NavBar</h2>
+      <div className="container nav__container">
+        <a href="index.html" className="nav__logo">
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className="nav__menu">
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <button id="theme__icon">
+          <IoMdColorPalette />
+        </button>
+      </div>
     </nav>
   );
 };
